7131a1c8818615c025bd87e319214e45de0cec06
Ryan Shappa <ryanshappa@gmail.com>
2024-08-08 15:01:05 -0500

    added auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,17 @@
 import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
-
+import { ClerkProvider} from '@clerk/nextjs'
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
-      <Component {...pageProps} />
-    </div>
+    <ClerkProvider {...pageProps}>
+      <div className={GeistSans.className}>
+        <Component {...pageProps} />
+      </div>
+    </ClerkProvider>
   );
 };
 
