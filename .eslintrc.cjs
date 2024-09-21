3d3a5c8ce6a4ec516f12cddec355e552dac73375
Ryan Shappa <ryanshappa@gmail.com>
2024-09-21 16:55:10 -0500

    another any lint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -72,6 +72,7 @@ const config = {
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-call': 'off',
     '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unsafe-argument': "warn",
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-var-requires': 'off',
