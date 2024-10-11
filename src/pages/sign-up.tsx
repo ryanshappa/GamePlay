432c4e21e3906c446c71836c660852f5e65d8b09
Ryan Shappa <ryanshappa@gmail.com>
2024-10-10 22:04:35 -0500

    fixing deployment

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,18 +1,12 @@
-// import { SignUp } from '@clerk/nextjs';
-// import { GetServerSideProps } from 'next';
-// import { auth } from '@clerk/nextjs/server';
+import React from 'react';
 
-// const SignUpPage = () => {
-//   return (
-//     <div className="flex items-center justify-center min-h-screen bg-gray-900">
-//       <div className="w-full max-w-md p-8 bg-gray-800 rounded shadow">
-//         <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-//       </div>
-//     </div>
-//   );
-// };
+const SignUpPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      {/* Your sign-up form or components */}
+    </div>
+  );
+};
 
-// // Specify that this page should not use the global Layout
-// SignUpPage.getLayout = (page: React.ReactElement) => page;
-
-// export default SignUpPage;
+export default SignUpPage;
